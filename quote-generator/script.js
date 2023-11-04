d5da584c21aad94a0b1316d617f31cceb24e7de0
keudaope <jyri.lindroos@keuda.fi>
2023-11-04 18:28:40 +0200

    2 ensimmäistä projektia

---
+++ quote-generator/script.js
@@ -0,0 +1,70 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const TwitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Loading Spinner Shown <-- Tarpeeton, koska nimi sama
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// Remove Loading Spinner <-- Tarpeeton, koska nimi sama
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+// Show New Quote
+function newQuote() {
+    showLoadingSpinner();
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    }
+    else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    }
+    else {
+        quoteText.classList.remove('long-quote');
+    }
+    /// Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    showLoadingSpinner();
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json(); 
+        //throw new Error('oops'); // testausta varten, että virhe tulee
+        newQuote();
+    } catch (error) {
+        //console.log(error);
+        getQuotes();
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+TwitterBtn.addEventListener('click', tweetQuote);
+// On Load
+getQuotes();
+showLoadingSpinner();
